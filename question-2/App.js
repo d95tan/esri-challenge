7130d78235a6c0b44bce22cfda9b1e748424ecb4
Dan Tan <d95tan@gmail.com>
2024-12-07 14:46:44 +0800

    feat: question 2 using react native

---
+++ question-2/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={require("./assets/map.html")}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  webview: {
+    flex: 1,
+  },
+});
